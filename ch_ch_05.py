44b82e79424e3f090f9e70f12ef015e33fbda540
Mateusz Zawadzki <matizawadzki@interia.pl>
2022-12-05 20:15:11 +0100

    Piąte zadanie eventowe

---
+++ ch_ch_05.py
@@ -0,0 +1,62 @@
+adres = [("Swiateczna",
+              "234",
+              5,
+              "Warszawa",
+              "02-326"),
+             ("Choinkowa",
+              "34A",
+              1,
+              "Krakow",
+              "31-326"
+              ),
+             ("Mikolaja Swietego",
+              "87",
+              3,
+              "Rzeszow",
+              "40-326"
+              ),
+             ("Bombkowa",
+              "214",
+              55,
+              "Gdansk",
+              "15-326"
+              ),
+             ("Prezentowa",
+              "543",
+              765,
+              "Warszawa",
+              "02-326"
+              ),
+             ("Serniczkowa",
+              "4B",
+              2,
+              "Krakow",
+              "02-326"
+              ),
+             ("Pierniczkowa",
+              "289",
+              55,
+              "Gdansk",
+              "02-326"
+              ),
+             ("Barszczykowa",
+              "234",
+              5,
+              "Rzeszow",
+              "37-326"
+              ),
+             ("Uszkowa",
+              "43H",
+              5,
+              "Wroclaw",
+              "02-326"
+              ),
+             ("Cukierkowa",
+              "23",
+              5,
+              "Poznan",
+              "02-326"
+              )]
+
+b = sorted(sorted(adres, key = lambda x : x[0]))
+print(b)
